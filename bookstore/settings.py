777510af046b7141ab5a43846df75bc6baacd1d2
Fryansb <ryan.sbv2@gmail.com>
2026-01-13 11:41:18 -0300

    Add Django REST Framework to INSTALLED_APPS - Exercise Module 4
    - Installed djangorestframework via poetry add
    - Executed poetry update
    - Added 'rest_framework' to INSTALLED_APPS
    - Project ready for REST API development
    
    Exercise completed as per EBAC Backend Python course requirements.

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django REST Framework - EBAC Exercise Module 4
     'rest_framework',
 ]
 
